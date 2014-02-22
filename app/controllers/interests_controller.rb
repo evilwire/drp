3426ef94a20c884f524fa56f238e56230af52e5d
Knight Fu <knight.w.fu@gmail.com>
2014-02-22 11:52:11 -0500

    made some changes, not sure what

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,13 +1,12 @@
 class InterestsController < ApplicationController
   def index
     interests = []
-    unless current_user.mentor_profile
+    if current_user.mentor_profile
        interests = current_user.mentor_profile.interests
     end
 
     respond_to do |format|
        format.json { render json: interests, status: :created }
     end
-  end 
-
+  end
 end
